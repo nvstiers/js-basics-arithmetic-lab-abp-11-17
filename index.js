98d61d8f169ce0ed7610a8cc71d124d9eac632a5
Nathaniel Stiers <nvstiers@gmail.com>
2017-11-10 20:51:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ newID = 1000000000 + oldID;
 ageIsValid = Number.isInteger(currentAge);
 randomNumber = Math.random() * 20;
 randomInteger = Math.floor(randomNumber);
+randomUserID = randomInteger + 1000000000;
